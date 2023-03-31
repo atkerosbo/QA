c12b496bd47d80f1515660804704071b778894c1
atkerosbo <atkerosbo@yahoo.com>
2023-03-31 21:41:09 +0200

    webdriver

--- radiomarija/funcsel.py
+++ radiomarija/funcsel.py
@@ -1,7 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
 
+
+domain = input()
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(PATH)
 
 driver.get("https://www.nordweb.rs/")
-print(driver.title)
+sleep(10)    
+driver.find_element(By.CLASS_NAME, "btn search").submit
+sleep(10)   
+driver.find_element(By.CLASS_NAME, "form-control").send_keys(domain)
+sleep(10)
+driver.find_element(By.CLASS_NAME, "recaptcha-checkbox-border").click
+sleep(10)
+driver.find_element(By.ID, "btnCheckAvailability").submit
+sleep(10)
+rezultat = driver.find_element(By.ID, "primaryLookupResult")
+sleep(10)
+
+
+print(rezultat)
