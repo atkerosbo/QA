0ec285eb5ce66b62f98ea579204f0d17b6513a05
atkerosbo <atkerosbo@yahoo.com>
2023-04-05 23:13:00 +0200

    05042023

---
+++ primarydectree/main.py
@@ -0,0 +1,18 @@
+class TreeNode:
+
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = TreeNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = TreeNode(value)
+            else:
+                self.right.insert(value)
